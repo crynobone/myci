37c17e66f6b89eabcc45cd86607b719e110028f5
crynobone <crynobone@2e988176-e793-11dd-ab07-b19b4dad1b3d>
2009-10-19 04:00:12 +0000

    Update application option & autoload config

--- trunk/system/application/config/autoload.php
+++ trunk/system/application/config/autoload.php
@@ -39,7 +39,7 @@
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database', 'template', 'authentication', 'parser');
+$autoload['libraries'] = array('database', 'template', 'authentication');
 
 
 /*
@@ -51,7 +51,7 @@ $autoload['libraries'] = array('database', 'template', 'authentication', 'parser
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('array', 'url', 'cookie', 'html', 'project');
+$autoload['helper'] = array('array', 'url', 'cookie', 'html', 'extci');
 
 
 /*
